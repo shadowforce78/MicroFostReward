4aed0ddba5f2d987772b92abd5dac76b48414127
shadowforce78 <65604210+shadowforce78@users.noreply.github.com>
2020-12-12 13:31:05 +0100

    Add files via upload

---
+++ Microfost Reward/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Microfost_Reward
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openSourceCodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scriptor7283ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Pusab", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1 search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.label1.Font = new System.Drawing.Font("Pusab", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(713, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You must me be connected to earn reward !!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Pusab", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(325, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "10 Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Pusab", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(577, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "50 Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 203);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 92);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(153, 203);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 24);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "You want to get some microsoft reward points ?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(153, 244);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(597, 24);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "You just need to click on how many reaserch you want :)";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.openSourceCodeToolStripMenuItem,
+            this.creditsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.contactToolStripMenuItem1});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.helpToolStripMenuItem.Text = " Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit    (alt + F4)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // contactToolStripMenuItem1
+            // 
+            this.contactToolStripMenuItem1.Name = "contactToolStripMenuItem1";
+            this.contactToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.contactToolStripMenuItem1.Text = "Contact";
+            this.contactToolStripMenuItem1.Click += new System.EventHandler(this.contactToolStripMenuItem1_Click);
+            // 
+            // openSourceCodeToolStripMenuItem
+            // 
+            this.openSourceCodeToolStripMenuItem.Name = "openSourceCodeToolStripMenuItem";
+            this.openSourceCodeToolStripMenuItem.Size = new System.Drawing.Size(118, 20);
+            this.openSourceCodeToolStripMenuItem.Text = "Open Source Code";
+            this.openSourceCodeToolStripMenuItem.Click += new System.EventHandler(this.openSourceCodeToolStripMenuItem_Click);
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.scriptor7283ToolStripMenuItem});
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            // 
+            // scriptor7283ToolStripMenuItem
+            // 
+            this.scriptor7283ToolStripMenuItem.Name = "scriptor7283ToolStripMenuItem";
+            this.scriptor7283ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.scriptor7283ToolStripMenuItem.Text = "scriptor#7283";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Microfost auto reward by scriptor#7283";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openSourceCodeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scriptor7283ToolStripMenuItem;
+    }
+}
+
---
+++ Microfost Reward/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Microfost_Reward
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.bing.com/search?q=one%20search");
+            Process.Start("https://www.bing.com/search?q=two%20search");
+            Process.Start("https://www.bing.com/search?q=three%20search");
+            Process.Start("https://www.bing.com/search?q=four%20search");
+            Process.Start("https://www.bing.com/search?q=five%20search");
+            Process.Start("https://www.bing.com/search?q=six%20search");
+            Process.Start("https://www.bing.com/search?q=seven%20search");
+            Process.Start("https://www.bing.com/search?q=eight%20search");
+            Process.Start("https://www.bing.com/search?q=nine%20search");
+            Process.Start("https://www.bing.com/search?q=ten%20search");
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                // const Random _random = new Random();         
+                // This method returns a random lowercase letter.
+                // ... Between 'a' and 'z' inclusize.
+                // int num = _random.Next(0, 26); // Zero to 25
+               // char let = (char)('a' + num);
+                Process.Start("https://www.bing.com/search?q=one%20search");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.bing.com/search?q=one%20search");
+            Process.Start("https://www.bing.com/search?q=two%20search");
+            Process.Start("https://www.bing.com/search?q=three%20search");
+            Process.Start("https://www.bing.com/search?q=four%20search");
+            Process.Start("https://www.bing.com/search?q=five%20search");
+            Process.Start("https://www.bing.com/search?q=six%20search");
+            Process.Start("https://www.bing.com/search?q=seven%20search");
+            Process.Start("https://www.bing.com/search?q=eight%20search");
+            Process.Start("https://www.bing.com/search?q=nine%20search");
+            Process.Start("https://www.bing.com/search?q=ten%20search"); 
+            Process.Start("https://www.bing.com/search?q=eleven%20search");
+            Process.Start("https://www.bing.com/search?q=twelve%20search");
+            Process.Start("https://www.bing.com/search?q=thirteen%20search");
+            Process.Start("https://www.bing.com/search?q=fourteen%20search");
+            Process.Start("https://www.bing.com/search?q=hentai");
+            Process.Start("https://www.bing.com/search?q=fifteen%20search");
+            Process.Start("https://www.bing.com/search?q=sixteen%20search");
+            Process.Start("https://www.bing.com/search?q=seventeen%20search");
+            Process.Start("https://www.bing.com/search?q=eighteen%20search");
+            Process.Start("https://www.bing.com/search?q=nineteen%20search");
+            Process.Start("https://www.bing.com/search?q=twenty%20search");
+            Process.Start("https://www.bing.com/search?q=subto%20search");
+            Process.Start("https://www.bing.com/search?q=scripteur78");
+            Process.Start("https://www.bing.com/search?q=scriptor#7283");
+            Process.Start("https://www.bing.com/search?q=scriptor#7283%20the%20best");
+            Process.Start("https://www.bing.com/search?q=XD");
+            Process.Start("https://www.bing.com/search?q=how%20to%20basic");
+            Process.Start("https://www.bing.com/search?q=tchoupi");
+            Process.Start("https://www.bing.com/search?q=pewdiepie");
+            Process.Start("https://www.bing.com/search?q=france");
+            Process.Start("https://www.bing.com/search?q=baguette");
+            Process.Start("https://www.bing.com/search?q=crevette");
+            Process.Start("https://www.bing.com/search?q=marmiton");
+            Process.Start("https://www.bing.com/search?q=discord");
+            Process.Start("https://www.bing.com/search?q=microfost");
+            Process.Start("https://www.bing.com/search?q=pull%20moche");
+            Process.Start("https://www.bing.com/search?q=minecraft");
+            Process.Start("https://www.bing.com/search?q=when%20fortnite%20will%20die");
+            Process.Start("https://www.bing.com/search?q=aaa");
+            Process.Start("https://www.bing.com/search?q=dsfffsw");
+            Process.Start("https://www.bing.com/search?q=<qd<gfrze");
+            Process.Start("https://www.bing.com/search?q=fgtrfgtrfg");
+            Process.Start("https://www.bing.com/search?q=ada");
+            Process.Start("https://www.bing.com/search?q=ajax");
+            Process.Start("https://www.bing.com/search?q=midas");
+            Process.Start("https://www.bing.com/search?q=pizza");
+            Process.Start("https://www.bing.com/search?q=tacos");
+            Process.Start("https://www.bing.com/search?q=discord.gg/UAHhBP6Zbk");
+            Process.Start("https://www.bing.com/search?q=okayy");
+            Process.Start("https://www.bing.com/search?q=Arceus4ever");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void contactToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/8YTCpaPED5");
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openSourceCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/shadowforce78/MicroFostReward");
+        }
+    }
+}
+
+
+
+// https://www.bing.com/search?q=
